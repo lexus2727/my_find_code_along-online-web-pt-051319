d7e551ed09eef618669ccb74f957a5b48c92a763
Candice Parker <lexus2424@gmail.com>
2019-05-19 13:55:15 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,26 @@
 require 'pry'
 
 def my_find(collection)
+#create counter
+i = 0
 
+#create while iterator
+#.length allows us to iterate through entire array
+while i < collection.length
+
+
+#yield the collection
+#if block returns true 
+  if yield(collection[i])
+
+#return the argument we passed into yield (the element in the array that evaluated true)
+  return collection[i]
+  
+#This will exit the loop and the element will be the return value of the method
 end
+
+#increment the counter
+  i += 1
+ end
+end
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 } 
